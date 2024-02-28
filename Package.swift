d7500cc5df163a64f5f0f7cd921ae1771c81ecad
Gabriel Sanzone <ext.gabriel.sanzone@farfetch.com>
2024-02-28 15:29:27 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,20 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "SANZONE-sdk-ios",
+    name: "SANZONE-sdk",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SANZONE-sdk-ios",
-            targets: ["SANZONE-sdk-ios"]),
+            name: "SANZONE-sdk",
+            targets: ["SANZONE-sdk"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SANZONE-sdk-ios"),
+            name: "SANZONE-sdk",
+            path: "Source"
+        ),
         .testTarget(
-            name: "SANZONE-sdk-iosTests",
-            dependencies: ["SANZONE-sdk-ios"]),
+            name: "SANZONE-sdk-Tests",
+            dependencies: ["SANZONE-sdk"]),
     ]
 )
