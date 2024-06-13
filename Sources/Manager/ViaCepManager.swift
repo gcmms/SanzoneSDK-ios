de5350e908bad4b3de900672320cf4a6501b49d7
Gabriel Sanzone <gabriel.sanzone-ext@intelipost.com.br>
2024-06-13 16:58:17 -0300

    Create ViaCepManager.swift

---
+++ Sources/Manager/ViaCepManager.swift
@@ -0,0 +1,44 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gabriel Sanzone on 13/06/24.
+//
+
+import Foundation
+
+
+struct ViaCepModel: Codable {
+    let cep, logradouro, complemento, bairro: String?
+    let localidade, uf, ibge, gia: String?
+    let ddd, siafi: String?
+}
+
+class ViaCepManager {
+    
+    func request(cep: String, completion: @escaping (ViaCepModel?, Error?) -> Void) {
+        let url = URL(string: "https://viacep.com.br/ws/\(cep)/json/")!
+
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "DataError", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])
+                completion(nil, error)
+                return
+            }
+            
+            do {
+                let viaCep = try JSONDecoder().decode(ViaCepModel.self, from: data)
+                completion(viaCep, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
+        
+        task.resume()
+    }
+}
