5e180a65b489264c7995b9f66e2c514d817a003f
Gabriel Sanzone <32177705+gcmms@users.noreply.github.com>
2025-08-02 10:42:02 -0300

    AppReviewManager fix

--- Sources/Manager/AppReviewManager.swift
+++ Sources/Manager/AppReviewManager.swift
@@ -15,29 +15,30 @@ public class AppReviewManager {
     
     /// Solicita a avaliação do app após um número definido de ativações. Só será exibido uma vez por instalação.
     /// - Parameter threshold: Número de vezes que a função precisa ser chamada antes de exibir a review. Default = 3.
+    @MainActor
     public static func requestReviewIfNeeded(threshold: Int = 3) {
         let defaults = UserDefaults.standard
-
-        // Verifica se já solicitou antes
+        
         if defaults.bool(forKey: hasReviewedKey) {
             return
         }
-
-        // Incrementa contador
+        
         var count = defaults.integer(forKey: launchCountKey)
         count += 1
         defaults.set(count, forKey: launchCountKey)
+        
+        guard count == threshold,
+              let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        else { return }
 
-        // Solicita avaliação se atingiu o limite
-        if count == threshold {
-            if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-                if #available(iOS 18.0, *) {
-                    AppStore.requestReview(in: scene)
-                } else {
-                    SKStoreReviewController.requestReview(in: scene)
-                }
+        if #available(iOS 18.0, *) {
+            Task {
+                await AppStore.requestReview(in: scene)
                 defaults.set(true, forKey: hasReviewedKey)
             }
+        } else {
+            SKStoreReviewController.requestReview(in: scene)
+            defaults.set(true, forKey: hasReviewedKey)
         }
     }
 }
