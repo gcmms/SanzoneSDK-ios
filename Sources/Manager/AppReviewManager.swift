1bde9536a1749886024853109c4027a75a63ba8f
Gabriel Sanzone <gabriel.sanzone-ext@intelipost.com.br>
2025-10-23 08:36:20 -0300

    Update AppReviewManager.swift

--- Sources/Manager/AppReviewManager.swift
+++ Sources/Manager/AppReviewManager.swift
@@ -30,8 +30,8 @@ public class AppReviewManager {
         guard count == threshold else { return }
 
         #if os(tvOS)
-        SKStoreReviewController.requestReview()
-        defaults.set(true, forKey: hasReviewedKey)
+        //SKStoreReviewController.requestReview()
+        //defaults.set(true, forKey: hasReviewedKey)
         #else
         guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
             return
